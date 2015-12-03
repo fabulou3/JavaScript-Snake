4d9757ac1770bf9cf33fde3b5c68ccffc1a316c4
Aniyoyo <jackgb888@gmail.com>
2015-12-03 12:28:10 +0800

    jaj the sppeed is 200 now
    i am still crap at javascript. yay.
    lets all party in the usa

--- js/snake.js
+++ js/snake.js
@@ -121,7 +121,7 @@ SNAKE.Snake = SNAKE.Snake || (function() {
             rowShift = [-1, 0, 1, 0],
             xPosShift = [],
             yPosShift = [],
-            snakeSpeed = 100,
+            snakeSpeed = 200,
             isDead = false,
             isPaused = false;
         function getMode (mode, speed) {
